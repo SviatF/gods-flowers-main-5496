1a9b7a668261595b39075937bb1025f374fc16ac
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-08-20 17:21:48 -0700

    Use verified embedded hero image chunks

--- packages/web/src/web/content/hero-image/index.ts
+++ packages/web/src/web/content/hero-image/index.ts
@@ -1,11 +1,12 @@
 import part1 from "./part1";
 import part2a from "./part2a";
 import part2b from "./part2b";
-import part3 from "./part3";
+import part3a from "./part3a";
+import part3b from "./part3b";
 import part4 from "./part4";
 import part5 from "./part5";
 import part6 from "./part6";
 
-const heroImage = `data:image/jpeg;base64,${part1}${part2a}${part2b}${part3}${part4}${part5}${part6}`;
+const heroImage = `data:image/jpeg;base64,${part1}${part2a}${part2b}${part3a}${part3b}${part4}${part5}${part6}`;
 
 export default heroImage;
