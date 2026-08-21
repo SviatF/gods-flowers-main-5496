66f8f167cadd955d814d591de21bf95daaaea039
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-08-20 17:19:43 -0700

    Fix embedded hero image data

--- packages/web/src/web/content/hero-image/index.ts
+++ packages/web/src/web/content/hero-image/index.ts
@@ -1,10 +1,11 @@
 import part1 from "./part1";
-import part2 from "./part2";
+import part2a from "./part2a";
+import part2b from "./part2b";
 import part3 from "./part3";
 import part4 from "./part4";
 import part5 from "./part5";
 import part6 from "./part6";
 
-const heroImage = `data:image/jpeg;base64,${part1}${part2}${part3}${part4}${part5}${part6}`;
+const heroImage = `data:image/jpeg;base64,${part1}${part2a}${part2b}${part3}${part4}${part5}${part6}`;
 
 export default heroImage;
